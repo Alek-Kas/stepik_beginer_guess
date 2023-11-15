405d4a945d452016d1522932ddbd3f0e1b56715d
Alek-Kas <spawn_kas@mail.ru>
2023-11-15 20:38:42 +0300

    0.02

--- main.py
+++ main.py
@@ -9,7 +9,15 @@ def is_valid(text):
 
 
 num1 = random.randint(1, 100)
-print("Добро пожаловать в числовую угадайку")
+print("Добро пожаловать в числовую угадайку!")
+print('Попробуйте угадать задуманное компьютером число от 1 до 100 включительно.')
 
-number = input()
-print(is_valid(number))
+number = 0
+while number != 'stop':
+    print('Введите "stop", чтобы закончить или число от 1 до 100: ', end='')
+    number = input()
+    if number == 'stop':
+        break
+    if is_valid(number):
+        print('Вы ввели не верное значение, повторите ввод!')
+        continue
