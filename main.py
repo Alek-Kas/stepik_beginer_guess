ae53074b4746616432d0e3191d27e52b8ac454d4
Alek-Kas <spawn_kas@mail.ru>
2023-11-15 20:53:50 +0300

    0.03 Организуйте сравнение введенного числа с загаданным числом: Если введенное число меньше загаданного числа, выведите текст: 'Ваше число меньше загаданного, попробуйте еще разок'; Если введенное число больше загаданного числа, выведите текст: 'Ваше число больше загаданного, попробуйте еще разок'; Если введенное число равно загаданному числу, выведите текст: 'Вы угадали, поздравляем!'. Выведите прощальное сообщение пользователю: 'Спасибо, что играли в числовую угадайку. Еще увидимся...'.

--- main.py
+++ main.py
@@ -8,16 +8,26 @@ def is_valid(text):
         return False
 
 
-num1 = random.randint(1, 100)
+rand_num = random.randint(1, 100)
 print("Добро пожаловать в числовую угадайку!")
 print('Попробуйте угадать задуманное компьютером число от 1 до 100 включительно.')
 
 number = 0
-while number != 'stop':
+while number != 'stop' and number != rand_num:
     print('Введите "stop", чтобы закончить или число от 1 до 100: ', end='')
     number = input()
     if number == 'stop':
         break
     if is_valid(number):
+        number = int(number)
+    else:
         print('Вы ввели не верное значение, повторите ввод!')
         continue
+    if number < rand_num:
+        print('Ваше число меньше загаданного, попробуйте еще разок')
+    else:
+        print('Ваше число больше загаданного, попробуйте еще разок')
+
+if number != 'stop':
+    print('Вы угадали, поздравляем!')
+print('Спасибо, что играли в числовую угадайку. Еще увидимся...')
